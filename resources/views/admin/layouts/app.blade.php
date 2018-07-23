9a5122f5918b2928c436ff2cf0e712d960c8c693
Quan Tran H <quan.tran2@asiantech.vn>
2018-07-23 23:56:34 +0700

    Admin delete clinic-type

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -9,6 +9,8 @@
     <!-- Custom styles for this template -->
     <link href="{{ asset('css/admin/dashboard.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="{{ asset('js/admin/dashboard.js') }}"></script>
   </head>
   <body>
     @include('admin.layouts.partials.navbar')
