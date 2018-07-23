762d523eec77b5f7fb8bf20ea6b8845465072261
Hai Nguyen V <hai.nguyen5@asiantech.vn>
2018-07-23 15:07:47 +0700

    add migration, model, seed for User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,11 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'gender' => $faker->boolean(),
+        'dob' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'phone' => $faker->tollFreePhoneNumber(),
+        'address' => $faker->address(),
+        'password' => bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
