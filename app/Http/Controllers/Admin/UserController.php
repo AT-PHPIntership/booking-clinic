fdbde1099e12fade6dd5374bc7aab70f7ece8fac
Hai Nguyen V <hai.nguyen5@asiantech.vn>
2018-07-23 17:31:23 +0700

    show all user

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::paginate();
+        return view('admin.users.index', compact('users'));
+    }
+}
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,46 @@
+@extends('admin.layouts.app')
+
+@section('title')
+  @lang('admin/user.title')
+@endsection
+
+@section('content')
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">@lang('admin/user.heading')</h1>
+  </div>
+  <div class="table-responsive">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>@lang('admin/user.fields.name')</th>
+          <th>@lang('admin/user.fields.email')</th>
+          <th>@lang('admin/user.fields.gender')</th>
+          <th>@lang('admin/user.fields.dob')</th>
+          <th>@lang('admin/user.fields.phone')</th>
+          <th>@lang('admin/user.fields.address')</th>
+          <th>@lang('admin/user.fields.created_at')</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($users as $user)
+          <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            @if ($user->gender === App\User::GENDER_MALE)
+              <td>@lang('admin/user.fields.gender_type.male')</td>
+            @else
+              <td>@lang('admin/user.fields.gender_type.female')</td>
+            @endif
+            <td>{{ $user->dob }}</td>
+            <td>{{ $user->phone }}</td>
+            <td>{{ $user->address }}</td>
+            <td>{{ $user->created_at }}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+    {{ $users->links() }}
+  </div>
+@endsection
