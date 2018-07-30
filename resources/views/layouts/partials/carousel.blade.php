61fed8cecaeb0b6a07ea3986c52ccb44e50853d2
at-longtran <long.tran@asiantech.vn>
2018-07-30 10:07:48 +0700

    adding end of file

--- resources/views/layouts/partials/carousel.blade.php
+++ resources/views/layouts/partials/carousel.blade.php
@@ -1,12 +1,12 @@
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="width">
   <ol class="carousel-indicators">
-      @foreach ($images as $key=>$image)
-  <li data-target="#carouselExampleIndicators" data-slide-to="{{ $key }}"{!! $key==0 ? ' class="active"' : '' !!}></li>
-      @endforeach
+    @foreach ($images as $key => $image)
+      <li data-target="#carouselExampleIndicators" data-slide-to="{{ $key }}" {!! $key==0 ? 'class="active"' : '' !!}></li>
+    @endforeach
   </ol>
   <div class="carousel-inner">
-    @foreach ($images as $key=>$image)
-      <div class="carousel-item{{ $key==0 ? ' active' : '' }}">
+    @foreach ($images as $key => $image)
+      <div class="carousel-item {{ $key==0 ? 'active' : '' }}">
         <img class="d-block w-100" src="{{ $image->path }}" alt="First slide">
       </div>
     @endforeach
