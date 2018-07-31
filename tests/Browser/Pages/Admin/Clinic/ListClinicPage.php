2916d2de103134c1902b7a4e9c648ffe090a019d
Hai Nguyen V <hai.nguyen5@asiantech.vn>
2018-07-31 10:31:24 +0700

    test listing all clinic

---
+++ tests/Browser/Pages/Admin/Clinic/ListClinicPage.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Browser\Pages\Admin\Clinic;
+
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\Page as BasePage;
+
+class ListClinicPage extends BasePage
+{
+    public const PER_PAGE = 15;
+    /**
+     * Get the URL for the page.
+     *
+     * @return string
+     */
+    public function url()
+    {
+        return '/admin/clinics';
+    }
+
+    /**
+     * Assert that the browser is on the page.
+     *
+     * @param  Browser  $browser
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser->assertPathIs($this->url())
+            ->assertSee('List clinics')
+            ->assertSee('Clinic Type')
+            ->assertSee('Name')
+            ->assertSee('Address')
+            ->assertSee('Phone');
+    }
+
+    /**
+     * Get the element shortcuts for the page.
+     *
+     * @return array
+     */
+    public function elements()
+    {
+        return [
+            '@element' => '#selector',
+        ];
+    }
+}
