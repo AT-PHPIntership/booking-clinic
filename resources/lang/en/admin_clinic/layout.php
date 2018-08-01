a938fdc876ebf149a0a1f468fd4ba3bf3bb792e3
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-02 00:17:15 +0700

    admin clinic dashboard page

---
+++ resources/lang/en/admin_clinic/layout.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'navbar' => [
+        'app_name' => 'Booking clinic',
+        'search' => 'Search',
+        'logout' => 'Logout',
+    ],
+    'sidebar' => [
+        'dashboard' => 'Dashboard',
+        'calendar' => 'Calendar',
+        'appointments' => 'Appointments',
+        'profile' => 'Profile'
+    ],
+    'message' => [
+        'success' => 'Add new item successfully'
+    ],
+    'btn' => [
+        'add' => 'Add',
+        'reset' => 'Reset',
+        'update' => 'Update',
+        'detail' => 'Detail'
+    ]
+];
---
+++ resources/views/admin_clinic/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('admin_clinic.layouts.app')
+
+@section('title')
+  @lang('admin_clinic/dashboard.title')
+@endsection
+
+@section('content')
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">@lang('admin_clinic/dashboard.heading')</h1>
+  </div>
+@endsection
