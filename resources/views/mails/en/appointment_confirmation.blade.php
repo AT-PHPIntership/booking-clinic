0a8de8b4c43fdcb3f0d286e24504fabd60f4dc8b
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-05 23:14:22 +0700

    change mail

--- resources/views/mails/en/appointment_confirmation.blade.php
+++ resources/views/mails/en/appointment_confirmation.blade.php
@@ -1,5 +1,7 @@
 <h1>{{ config('app.name') }}</h1>
 <strong>Greeting!</strong>
 <p>Hello {{$appointment->user->name}}</p>
-<p>Your appointment Id {{ $appointment->id }} book in {{ $appointment->book_time }} is {{$appointment->status}} </p>
-<em>From {{ $appointment->clinic->name }}</em>
+<p>Your appointment # {{ $appointment->id }} book in
+  {{ $appointment->book_time->day . '/' . $appointment->book_time->month . '/' . $appointment->book_time->year }}
+  on {{ $appointment->book_time->hour . ':' . $appointment->book_time->hour . ':' . $appointment->book_time->second }} is {{$appointment->status}} </p>
+<em>From {{ $appointment->clinic->name }} clinic</em>
