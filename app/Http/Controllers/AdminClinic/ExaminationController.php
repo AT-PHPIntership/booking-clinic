61936fa3ee2c6afe3184f99431c6830b56459968
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-09 16:01:24 +0700

    okkkk

--- app/Http/Controllers/AdminClinic/ExaminationController.php
+++ app/Http/Controllers/AdminClinic/ExaminationController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\AdminClinic;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\AdminClinic\BaseController;
+use App\Appointment;
 
-class ExaminationController extends Controller
+class ExaminationController extends BaseController
 {
-    //
+    /**
+     * Store a examination of clinic
+     *
+     * @param \Illuminate\Http\Requests $request     request
+     * @param String                    $slug        slug
+     * @param \App\Appointment          $appointment appointment
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $slug)
+    {
+        $status = Appointment::STATUS;
+        if ($this->clinic->id == $appointment->id and $status[$appointment->status] == $status['Pending']) {
+            $data = $request->all();
+            $data['appointment_id'] = $appointment->id;
+            \App\Examination::create($data);
+            $appointment->update(['status' => $status['Completed']]);
+            return response()->json(204);
+        }
+        unset($slug);
+        return response()->json(400);
+
+    }
 }
--- public/js/admin_clinic/examination.js
+++ public/js/admin_clinic/examination.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
     $("#create-examination").click(function(e) {
+      slug = $('input[type="hidden"]').data("slug");
+      appointmentId = $('input[type="hiden"]').data("appointmentId");
+      debugger;
       $.confirm({
         columnClass: 'col-md-8',
         title: '<div class="h2">Examination patient ' + $("#user-name").val() + '</div>',
@@ -21,11 +24,36 @@ $(document).ready(function() {
             action: function () {
               var diagnostic = this.$content.find('input[name="diagnostic"]').val();
               var result = this.$content.find('textarea[name="result"]').val();
-            //   if(!name){
-            //     $.alert('provide a valid name');
-            //     return false;
-            //   }
-              console.log(result);
+              if(!diagnostic.trim()) {
+                $.alert('provide a valid name');
+                return false;
+              }
+
+              //   Call Ajax create examination
+              $.ajaxSetup({
+                  headers: {
+                    'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content'),
+                  }
+                });
+              $.ajax({
+                url: '/admin/' + slug + '/examinations',
+                type: 'POST',
+                cache: false,
+                data: {diagnostic: diagnostic, result: result, },
+              })
+              .done(function (res) {
+
+                // delete sync icon and show success message in 2 seconds, remove option Confirmed when chose option Cancel
+                ele.next().remove();
+                ele.after('<div class="pr-5 pl-5 d-inline alert alert-success status-flash">The status is updated</div>');
+                ele.next().slideUp(2000);
+                setTimeout(function() {
+                  ele.next().remove();
+                }, 2000);
+                ele.children("option:nth-child(1)").remove();
+              });
+
+
             }
           },
           cancel: function () {
