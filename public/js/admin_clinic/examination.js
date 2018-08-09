a774702873ba669d36c907c0a6e4e0f81c5eff42
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-10 03:15:20 +0700

    show collapse

--- public/js/admin_clinic/examination.js
+++ public/js/admin_clinic/examination.js
@@ -1,22 +1,26 @@
+const COMPLETED = 'Completed'
 $(document).ready(function() {
 
     $("#create-examination").click(function(e) {
+        ele = $(this);
         var slug = $("#slug").val();
-        appointmentId = $('.status-select').attr('id').substring(12);
+        select = $('.status-select')
+        userName = $("#user-name").val()
+        appointmentId = select.attr('id').substring(12);
         $.confirm({
             columnClass: 'col-md-8',
             icon: 'fa fa-spinner fa-spin',
-            title: '<div class="h4 mb-0">Examination patient ' + $("#user-name").val() + '</div>',
-            content: '<form action="" class="formName">' +
-            '<div class="form-group">' +
-                '<label>Diagnostic</label>' +
-                '<input type="text" placeholder="Diagnostic" name="diagnostic" class="name form-control" required />' +
-            '</div>' +
-            '<div class="form-group">' +
-                '<label>Result</label>' +
-                '<textarea class="form-control" name="result" required></textarea>' +
-            '</div>' +
-            '</form>',
+            title: '<div class="h4 mb-0">Examination patient ' + userName + '</div>',
+            content: '<form action="" class="formName">\
+                        <div class="form-group">\
+                            <label>Diagnostic</label>\
+                            <input type="text" placeholder="Diagnostic" name="diagnostic" class="name form-control" required />\
+                        </div>\
+                        <div class="form-group">\
+                            <label>Result</label>\
+                            <textarea class="form-control" name="result" required></textarea>\
+                        </div>\
+                    </form>',
             buttons: {
                 formSubmit: {
                     text: 'Submit',
@@ -43,17 +47,33 @@ $(document).ready(function() {
                     })
                     .done(function (res) {
                         console.log(JSON.stringify(res));
-                        // delete sync icon and show success message in 2 seconds, remove option Confirmed when chose option Cancel
-                        // ele.next().remove();
-                        // ele.after('<div class="pr-5 pl-5 d-inline alert alert-success status-flash">The status is updated</div>');
-                        // ele.next().slideUp(2000);
-                        // setTimeout(function() {
-                        // ele.next().remove();
-                        // }, 2000);
-                        // ele.children("option:nth-child(1)").remove();
+                        select.css("background-color", "#28a745");
+                        select.children().remove();
+                        select.append('<option value="2" selected>' + COMPLETED + '</option>')
+                        ele.after('<div class="col-sm-10">\
+                                    <a class="btn btn-success col-md-3" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">\
+                                        Show examination\
+                                    </a>\
+                                    <div class="collapse" id="collapseExample">\
+                                        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">\
+                                            <h2 class="h2">Examination ' + userName + '</h2>\
+                                        </div>\
+                                        <div class="form-group">\
+                                            <label>Diagnostic</label>\
+                                            <input type="text" name="diagnostic" class="name form-control" value="' + res.diagnostic + '" readonly />\
+                                        </div>\
+                                        <div class="form-group">\
+                                            <label>Result</label>\
+                                            <textarea class="form-control" name="result" readonly>' + res.result + '</textarea>\
+                                        </div>\
+                                        <div class="form-group">\
+                                            <label>Created at</label>\
+                                            <textarea class="form-control" name="created_at" readonly>' + res.created_at + '</textarea>\
+                                        </div>\
+                                    </div>\
+                                </div>');
+                        ele.remove();
                     });
-
-
                     }
                 },
             cancel: function () {
