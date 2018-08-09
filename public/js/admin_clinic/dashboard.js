0646803bd5c6bf07f8641518aa0598c64a3181c8
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-09 17:42:40 +0700

    axxxx

--- public/js/admin_clinic/dashboard.js
+++ public/js/admin_clinic/dashboard.js
@@ -1,19 +1,7 @@
 const CONFIRMED = 'Confirmed';
 const CANCEL = 'Cancel';
 
-function updateStatusDone(ele, value) {
-    // Remove 2 button and hide appointment is changed status
-    ele.closest("tr").addClass("delete");
-    if (value == '.accept') {
-      ele.prev().css("background-color", '#007bff').val(CONFIRMED).next().next().remove();
-    } else {
-      ele.prev().prev().css("background-color", '#dc3545').val(CANCEL).next().remove();
-    }
-    ele.remove();
-    $(".delete").fadeOut(3000);
-}
-
-function updateStatus(slug, appointmentId, status, ele, value) {
+function updateStatus(slug, appointmentId, status, callback) {
   $.ajax({
     url: '/admin/' + slug + '/appointments/' + appointmentId,
     type: 'PATCH',
@@ -21,7 +9,7 @@ function updateStatus(slug, appointmentId, status, ele, value) {
     data: {status: status},
   })
   .done(function (res) {
-    updateStatusDone(ele, value);
+    callback();
   });
 };
 
@@ -30,31 +18,76 @@ $(document).ready(function() {
     $(this).css("background-color", '#ffc107');
   });
   var slug = $("#slug").val();
-  $.each({1: ".accept", 3: ".cancel"}, function (index, value) {
-    $(value).click(function(e) {
+
+    $(".accept").click(function(e) {
       var ele = $(this);
       var appointmentId = ele.attr('id').substring(7);
-      var status = index;
+      var status = '1';
       $.ajaxSetup({
         headers: {
           'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content'),
         }
       });
       e.preventDefault();
-      if (value == '.cancel') {
-        $.confirm({
-          title: 'Appointments!',
-          theme: 'dark',
-          content: 'Do you want cancel this appointment!',
-          buttons: {
-            confirm: function () {
-              updateStatus(slug, appointmentId, status, ele, value);
-            },
-            cancel: function () {},
-          }
-        });
-        }
-      else updateStatus(slug, appointmentId, status, ele, value);
+      var doneStatus = function() {
+
+        // Remove 2 button and hide appointment is changed status
+        ele.closest("tr").addClass("delete");
+        // if (value == '.accept') {
+          ele.prev().css("background-color", '#007bff').val(CONFIRMED).next().next().remove();
+        // } else {
+        //   ele.prev().prev().css("background-color", '#dc3545').val(CANCEL).next().remove();
+        // }
+        ele.remove();
+        $(".delete").fadeOut(3000);
+      };
+      // if (value == '.cancel') {
+      //   $.confirm({
+      //     title: 'Appointments!',
+      //     theme: 'dark',
+      //     content: 'Do you want cancel this appointment!',
+      //     buttons: {
+      //       confirm: function () {
+      //         updateStatus(slug, appointmentId, status, doneStatus);
+      //       },
+      //       cancel: function () {},
+      //     }
+      //   });
+      //   }
+      // else
+      updateStatus(slug, appointmentId, status, doneStatus);
     });
-  })
+
+    $(".cancel").click(function(e) {
+      var ele = $(this);
+      var appointmentId = ele.attr('id').substring(7);
+      var status = '0';
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content'),
+        }
+      });
+      $.confirm({
+        title: 'Appointments!',
+        theme: 'dark',
+        content: 'Do you want cancel this appointment!',
+        buttons: {
+          confirm: function () {
+            updateStatus(slug, appointmentId, status, doneStatus);
+          },
+          cancel: function () {},
+        }
+      });
+      e.preventDefault();
+      var doneStatus = function() {
+        // Remove 2 button and hide appointment is changed status
+        ele.closest("tr").addClass("delete");
+        ele.prev().prev().css("background-color", '#dc3545').val(CANCEL).next().remove();
+        ele.remove();
+        $(".delete").fadeOut(3000);
+        debugger;
+
+      };
+    });
+
 });
