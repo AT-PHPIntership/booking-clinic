fd738f8f3258198cff1e81a043f1ad0df391f609
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-09 12:30:03 +0700

    add examination controller

---
+++ app/Http/Controllers/AdminClinic/ExaminationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\AdminClinic;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ExaminationController extends Controller
+{
+    //
+}
