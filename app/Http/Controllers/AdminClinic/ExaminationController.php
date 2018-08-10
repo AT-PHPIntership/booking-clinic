a79a224900f9ead35cddca4814720dc209c5af13
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-10 14:15:04 +0700

    ok mail

--- app/Http/Controllers/AdminClinic/ExaminationController.php
+++ app/Http/Controllers/AdminClinic/ExaminationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\AdminClinic;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\AdminClinic\BaseController;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\AppointmentConfirmationEmail;
 use App\Appointment;
 
 class ExaminationController extends BaseController
@@ -21,16 +23,15 @@ class ExaminationController extends BaseController
     {
         $appointment = Appointment::findOrFail($request->appointmentId);
         if ($this->clinic->id == $appointment->clinic->id and $appointment->isStatus('Confirmed')) {
-            try{
                 $data = $request->all();
                 $data['appointment_id'] = $appointment->id;
                 \App\Examination::create($data);
                 $appointment->update(['status' => Appointment::STATUS['Completed']]);
+                $mesage = (new AppointmentConfirmationEmail($appointment))
+                    ->onQueue('emails');
+                Mail::to($appointment->user->email)
+                    ->queue($mesage);
                 return response()->json( \App\Examination::latest()->first(), 200);
-            } catch(\Exception $e) {
-                return response()->json($e, 200);
-            }
-
         }
         unset($slug);
         return response()->json(400);
