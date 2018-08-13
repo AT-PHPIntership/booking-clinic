8f964e4e2c10fc9bc24fd52831f830ebae0e514b
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-13 10:22:47 +0700

    done api register

--- app/helpers.php
+++ app/helpers.php
@@ -13,3 +13,18 @@ if (! function_exists('checkRouteActive')) {
         return strpos(request()->url(), $route) !== false ? ' active' : '';
     }
 }
+
+if (! function_exists('getPerPage')) {
+    /**
+     * Check request to get per_page query
+     *
+     * @return string
+     */
+    function getPerPage()
+    {
+        if (request()->has('perpage')) {
+            return request()->perpage;
+        }
+        return config('define.limit_rows');
+    }
+}
