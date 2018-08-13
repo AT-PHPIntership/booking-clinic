4d36a823e2cf12251b1f4e5c717363fc08b5facd
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-13 10:22:47 +0700

    done api register

--- config/define.php
+++ config/define.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
     'access_token' => 'Personal Access Token',
     'limit_rows' => 15
---
+++ tests/Feature/API/UserLogoutTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\API;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Passport\Passport;
+
+class UserLogoutTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp() {
+        parent::setUp();
+        Passport::actingAs(factory(\App\User::class)->create());
+    }
+
+    /**
+     * Test user logout.
+     *
+     * @return void
+     */
+    public function test_user_logout_success()
+    {
+        $response = $this->json('POST', '/api/logout');
+        $response->assertStatus(200)
+            ->assertJson([
+                "result" => [
+                "Successfully logged out"
+                ],
+                "code" => 200
+            ]);
+    }
+}
