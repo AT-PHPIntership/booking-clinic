8756fda3979bf9b2c348d564e76d25f7dd83e635
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-16 12:01:52 +0700

    test user register

--- tests/Feature/API/UserRegisterTest.php
+++ tests/Feature/API/UserRegisterTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Hash;
 
 class UserRegisterTest extends TestCase
 {
@@ -13,6 +14,11 @@ class UserRegisterTest extends TestCase
 
     public function setUp() {
         parent::setUp();
+        factory(\App\User::class)->create([
+            'name' => 'userName',
+            'email' => 'quan@gmail.com',
+            'password' => Hash::make('secret')
+        ]);
     }
     public function json_structure_user_register_success() {
         return [
@@ -20,9 +26,17 @@ class UserRegisterTest extends TestCase
             'code'
         ];
     }
+    public function json_structure_user_register_fail() {
+        return [
+            'message',
+            'errors',
+            'code',
+            'request'
+        ];
+    }
 
     /**
-     * A basic test example.
+     * Test user register success.
      *
      * @return void
      */
@@ -31,13 +45,68 @@ class UserRegisterTest extends TestCase
         $body = [
             "name" => "hongquan1234",
             "email" => "hongquan1234@gmail.com",
+            "gender" => 0,
+            "dob" => "1995-02-22",
+            'phone' => '0123456789',
+            'address' => 'DA Nang',
             "password" => "secret",
             "password_confirmation" => "secret"
 
         ];
-        $response = $this->json('POST', '/api/register', $body, ['Accept' => 'application/json']);
-        // dd($response);
+        $response = $this->json('POST', '/api/register', $body);
         $response->assertStatus(201)
             ->assertJsonStructure($this->json_structure_user_register_success());
+        $this->assertDatabaseHas('users', [
+            "email" => "hongquan1234@gmail.com"
+        ]);
+    }
+
+     /**
+     * list test case validate input user register
+     * @param
+     * @return array
+     */
+    public function list_test_case_validate_input_user_register()
+    {
+        return [
+            [
+                [
+                    "name" => "",
+                    "email" => "",
+                    "password" => "secret",
+                    "password_confirmation" => "secret"
+                ],
+                [
+                    "name",
+                    "email"
+                ]
+            ],
+            [
+                [
+                    "name" => "userName",
+                    "email" => "quan@gmail.com",
+                    "password" => "secret",
+                    "password_confirmation" => "secrettt"
+                ],
+                [
+                    "email",
+                    "password"
+                ]
+            ],
+
+        ];
+    }
+
+    /**
+     * @param string body
+     * @param string field
+     * @dataProvider list_test_case_validate_input_user_register
+     */
+    public function test_user_register_fail($body, $field)
+    {
+        $response = $this->json('POST', '/api/register', $body);
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors($field)
+            ->assertJsonStructure($this->json_structure_user_register_fail());
     }
 }
