79e4356e85882a87a3d794f1311ea42515f2e745
Quan Tran H <quan.tran2@asiantech.vn>
2018-08-29 10:34:14 +0700

    show examination result

--- public/js/user/appointments_index.js
+++ public/js/user/appointments_index.js
@@ -28,7 +28,10 @@ function showAppointments(appointments) {
 
     let currentStatus = appointment.status;
     if (currentStatus == STATUS_LABELS_LANG.pending || currentStatus == STATUS_LABELS_LANG.confirmed) {
-      currentAppointment.find('.cancel-button').toggleClass('invisible');
+      currentAppointment.find('.cancel-button').toggleClass('d-none');
+    }
+    if (currentStatus == STATUS_LABELS_LANG.completed) {
+        currentAppointment.find('.result-button').toggleClass('d-none');
     }
 
     currentAppointment.find('.status').text(currentStatus)
@@ -87,25 +90,63 @@ function cancelAppointment(id) {
     $.alert({
       title: Lang.get('user/appointment.cancel.title'),
       theme: 'material',
+      type:'red',
+      typeAnimated: true,
       content: res.responseJSON.error,
     });
   });
 }
 
+// Cancel pending or confirmed appointment.
 function cancelAppointmentSuccess(res) {
-    let currentId = res.result.id;
-    let currentStatus = res.result.status;
+  let currentId = res.result.id;
+  let currentStatus = res.result.status;
 
-    $(`#appointment-${currentId} .cancel-button`).addClass('invisible');
-    $(`#appointment-${currentId} .status`)
-      .addClass(`btn-${STATUS_COLOR[$.inArray(currentStatus, STATUS_LABELS)]}`)
-      .text(currentStatus);
+  $(`#appointment-${currentId} .cancel-button`).toggleClass('d-none');
+  $(`#appointment-${currentId} .status`)
+    .addClass(`btn-${STATUS_COLOR[$.inArray(currentStatus, STATUS_LABELS)]}`)
+    .text(currentStatus);
 }
 
-$(document).ready(function () {
-  getAppointments();
+// Show examination result completed appointment
+function showResult(id)
+{
+  const token = getToken();
+  $.ajax({
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer '  + token
+    },
+    url: route('api.appointments.result', id),
+    type: 'GET'
+  })
+  .done(showResultSuccess)
+  .fail(function(res) {
+    $.alert({
+      title: Lang.get('user/appointment.result.title'),
+      theme: 'material',
+      type:'red',
+      typeAnimated: true,
+      content: res.responseJSON.error,
+    });
+  });
+}
 
-  // Redirect page using ajax
+function showResultSuccess(res)
+{
+  $('input[name="clinic-name"]').attr('value', res.result.clinic.name);
+  $('input[name="clinic-email"]').attr('value', res.result.clinic.email);
+  $('input[name="clinic-address"]').attr('value', res.result.clinic.address);
+  $('input[name="user-name"]').attr('value', res.result.user.name);
+  $('input[name="user-email"]').attr('value', res.result.user.email);
+  $('input[name="diagnostic"]').attr('value', res.result.examination.diagnostic);
+  $('input[name="created-at"]').attr('value', res.result.examination.created_at);
+  $('textarea[name="descsription"]').text(res.result.examination.descsription);
+}
+
+// Redirect page using ajax
+function clickPaginate()
+{
   $(document).on('click', '.page-link', function (e) {
     e.preventDefault();
     let nextPageHref = $(this).attr("href");
@@ -115,10 +156,10 @@ $(document).ready(function () {
     history.pushState({}, '', window.location.pathname + nextPageQuery); //Set query to URL before call Ajax
     getAppointments();
   });
+}
 
-  filter();
-
-  // Cancel pending or confirmed appointment.
+function clickCancel()
+{
   $(document).on('click', '.cancel-button', function (e) {
     e.preventDefault();
     let appointmentId = $(this).closest('.appointment').attr('id');
@@ -143,4 +184,36 @@ $(document).ready(function () {
       }
     });
   });
+}
+
+function clickShowResult()
+{
+  $(document).on('click', '.result-button', function (e) {
+    e.preventDefault();
+    let appointmentId = $(this).closest('.appointment').attr('id');
+
+    appointmentId = appointmentId.split('-')[1];
+    $.alert({
+      title: Lang.get('user/appointment.result.title'),
+      theme: 'material',
+      type: 'green',
+      columnClass: 'col-lg-8',
+      backgroundDismiss: true,
+      content: showExamiantionResult(),
+      onContentReady: showResult(appointmentId),
+    });
+  });
+}
+
+$(document).ready(function () {
+  getAppointments();
+  clickPaginate();
+  filter();
+  clickCancel();
+  clickShowResult();
+});
+
+$(document).on('click', '.show-more', function(e) {
+    e.preventDefault();
+    $(this).prev().toggleClass('des')
 });
--- resources/lang/en/user/appointment.php
+++ resources/lang/en/user/appointment.php
@@ -4,10 +4,22 @@ return [
     'book_date' => 'Book date: ',
     'time' => 'Time: ',
     'cancel_button' => 'Cancel',
+    'result_button' => 'Result',
     'detail' => 'Detail',
     'cancel' => [
         'confirm' => 'Do you want cancel this appointment?',
         'title' => 'Appointment',
         'error' => 'Error',
-    ]
+    ],
+    'result' => [
+        'title' => 'Examiantion result',
+        'clinic' => 'Clinic',
+        'email' => 'email',
+        'clinic_address' => 'Clinic address',
+        'patient' => 'Patient',
+        'diagnostic' => 'Diagnostic',
+        'created_at' => 'Created at',
+        'description' => 'Description'
+    ],
+    'show_more' => 'Show more'
 ];
