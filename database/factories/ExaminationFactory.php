e0e5982fb88a5541a86c10418c9b59d9fc85547d
Quan Tran H <quan.tran2@asiantech.vn>
2018-09-02 23:41:20 +0700

    add to database Seeder

--- database/factories/ExaminationFactory.php
+++ database/factories/ExaminationFactory.php
@@ -6,6 +6,7 @@ $factory->define(App\Examination::class, function (Faker $faker) {
     return [
         'diagnostic' => $faker->sentence,
         'result' => $faker->text,
+        'description' => $faker->text,
         'appointment_id' => function() {
             return factory(App\Appointment::class)->create()->id;
         }
