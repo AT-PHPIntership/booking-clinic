fb8f63aff68629ffff21a4d2b242caa63f97e155
Quan Tran H <quan.tran2@asiantech.vn>
2018-09-04 13:51:29 +0700

    add js path to admin page

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <link href="{{ asset('css/admin/dashboard.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="{{ asset('js/admin/message.js') }}"></script>
     <script src="{{ asset('js/admin/dashboard.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
   </head>
